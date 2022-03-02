1e2dd7298b011c6f136c5760b773d3a66796c437
Dmitriy <devilins24@gmail.com>
2022-03-02 15:15:03 +0200

    add 2nd countdown launch section html,css.js

--- script.js
+++ script.js
@@ -14,14 +14,18 @@ const months = [
 ];
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-// end of giveway date
+// temp date
 const tempDate = new Date();
 const tempYear = tempDate.getFullYear();
 const tempMonth = tempDate.getMonth();
 const tempDay = tempDate.getDate();
 
+// giveaway date
 const fullEndDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 const fullEndDateTimeInMs = fullEndDate.getTime();
+// launch date
+const fullLaunchDate = new Date(tempYear, tempMonth + 2, tempDay + 10, 6, 00, 0);
+const fullLaunchDateTimeInMs = fullLaunchDate.getTime();
 
 let endDateYear = fullEndDate.getFullYear();
 let endDateMonth = fullEndDate.getMonth();
@@ -36,11 +40,16 @@ const endDateTextSection = document.querySelector(".info h4");
 
 endDateTextSection.textContent = `Giveaway ends on ${weekDayText}, ${endDateMonth} ${endDateDay} ${endDateYear} at ${endDateHour}:${endDateMinutes}am`;
 
+// giveaway elements
 const countdownNumberElements = document.querySelectorAll(".countdown-number h4");
+// launch time elements
+const launchCountdownNumberElements = document.querySelectorAll(".launch-countdown-number h4");
 
+// calculate timer func
 const calculateRemainingTime = () => {
   const todayInMs = new Date().getTime();
   const timeDifference = fullEndDateTimeInMs - todayInMs; // time difference between dates in ms
+  const launchTimeDifference = fullLaunchDateTimeInMs - todayInMs;
 
   // 1s = 1000ms
   // 1m = 60s
@@ -52,15 +61,21 @@ const calculateRemainingTime = () => {
   const oneHour = 60 * 60 * 1000;
   const oneDay = 24 * 60 * 60 * 1000;
 
-  // calculate all values
-  let days = timeDifference / oneDay;
-  days = Math.floor(days);
+  // giveaway calculate all values
+  let days = Math.floor(timeDifference / oneDay);
   let hours = Math.floor((timeDifference % oneDay) / oneHour);
   let minutes = Math.floor((timeDifference % oneHour) / oneMinute);
   let seconds = Math.floor((timeDifference % oneMinute) / 1000);
 
+  // launch calcualte all values
+  let launchDays = Math.floor(launchTimeDifference / oneDay);
+  let launchHours = Math.floor((launchTimeDifference % oneDay) / oneHour);
+  let launchMinutes = Math.floor((launchTimeDifference % oneHour) / oneMinute);
+  let launchSeconds = Math.floor((launchTimeDifference % oneMinute) / 1000);
+
   // set number values array
-  const values = [days, hours, minutes, seconds];
+  const giveawayValues = [days, hours, minutes, seconds];
+  const launchValues = [launchDays, launchHours, launchMinutes, launchSeconds];
 
   // format number to 2 digits func
   function format(item) {
@@ -72,7 +87,11 @@ const calculateRemainingTime = () => {
 
   // insert countdown numbers
   countdownNumberElements.forEach((num, index) => {
-    num.innerHTML = format(values[index]);
+    num.innerHTML = format(giveawayValues[index]);
+  });
+  // insert launch countdown numbers
+  launchCountdownNumberElements.forEach((num, index) => {
+    num.innerHTML = format(launchValues[index]);
   });
 
   // check if giveaway is over
